5693aededd0f76ec920c939113888b8acceea114
wangjian <wangjian@eazytec.com>
2022-11-08 18:13:36 +0800

    【update】

---
+++ backend/controllers/auth.js
@@ -0,0 +1,36 @@
+import { db } from "../server/connect"
+import bcrypt from "bcrypt"
+
+export const register = (req,res)=>{
+
+    const q = "SELECT FROM users WHERE username = ?"
+    db.query(q,[req.body.username],(err,data) => {
+        if(err) return res.status(500).json(err)
+        if(data.length) return res.status(409).json("user already exists!")
+
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(req.body.password,salt)
+    
+        const q = "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)"
+        const values = [req.body.username, req.body.email, hashedPassword,req.body.name]
+
+        db.query(q,[values],(err,data)=>{
+            if(err) return res.status(500).json(err)
+            return res.status(200).json("user has been created")
+        })
+
+    })
+
+
+
+}
+
+export const login = (req,res)=>{
+
+
+}
+
+export const logout = (req,res)=>{
+
+
+}
---
+++ backend/server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+import authRoutes from "../routes/auth"
+import userRoutes from "../routes/users"
+import postRoutes from "../routes/posts"
+import commentRoutes from "../routes/comments"
+import likeRoutes from "../routes/likes"
+
+
+app.use(express.json())
+
+app.use("/api/auth",authRoutes)
+app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
+app.use("/api/comments",commentRoutes)
+app.use("/api/likes",likeRoutes)
+
+
+
+app.listen(8800,()=>{
+    console.log("API working!")
+})
+
